be43b727315e09928e531f79e29ca46d97cb57dc
Andrew Thauer <6507159+andrewthauer@users.noreply.github.com>
2019-09-07 14:37:35 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Dotfiles CLI
+
+This is a simple reference implementation of a [rbenv](https://github.com/rbenv/rbenv)
+inspired CLI for managing dotfiles.
+
+## Features
+
+- usage of `libexec` for sub commands
+- `bash` & `zsh` completions
+- manual file support
---
+++ init.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+DIR="${DOTFILES_DIR}/dotfiles"
+
+if [[ -n "${ZSH_NAME}" ]]; then
+  # Ensure dotfiles cmd is in path
+  if [[ ! $+commands[dotfiles] ]]; then
+    path=("${DIR}/bin" $path)
+    fpath=("${DIR}/bin" $fpath)
+  fi
+
+  # Setup completions
+  source "${DIR}/completions/dotfiles.zsh"
+else
+  # Setup completions
+  source "${DIR}/completions/dotfiles.bash"
+fi
+
+# shortcut aliases
+alias dot="dotfiles"
+alias doti="dotfiles init"
